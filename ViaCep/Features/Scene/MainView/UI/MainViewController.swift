cdee9fde96bc86d1797ded0cf80c3ba8f19bc359
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2023-08-05 17:44:35 -0300

    Refactor(Layout configuration):
    - Did add private key word.

--- ViaCep/Features/Scene/MainView/UI/MainViewController.swift
+++ ViaCep/Features/Scene/MainView/UI/MainViewController.swift
@@ -177,7 +177,7 @@ extension MainViewController {
     }
 }
 
-extension MainViewController.Layout {
+private extension MainViewController.Layout {
     enum Size {
         static let constant: CGFloat = 16
         static let height: CGFloat = 40
