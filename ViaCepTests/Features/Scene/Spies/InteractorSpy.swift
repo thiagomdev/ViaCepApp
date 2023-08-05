99a751103f333bf24485bff179e8247ef3f5d54f
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2023-08-05 17:47:56 -0300

    Refactor(Editing Spy):
    - Did create enum of messages.
    
    - Did create variable of messages.
    
    - Did append values.

--- ViaCepTests/Features/Scene/Spies/InteractorSpy.swift
+++ ViaCepTests/Features/Scene/Spies/InteractorSpy.swift
@@ -2,27 +2,27 @@ import Foundation
 @testable import ViaCep
 
 final class InteractorSpy: MainPresenting {
+    enum Messages: Equatable {
+        case presentCep(_ cep: ViaCep.DataCep)
+        case displayError(_ message: String)
+        case displayInvalidCepAlertMessage(_ data: ViaCep.DataCep)
+    }
     
+    private(set) var messages: [Messages] = []
     private(set) var expected: String?
     private(set) var dataModelExpected: DataCep?
-    private(set) var wasCalled: Bool =  false
-    private(set) var howManyTimes: Int = 0
         
     func presentCep(_ cep: ViaCep.DataCep) {
-        wasCalled = true
-        howManyTimes += 1
-        expected = cep.cep
+        messages.append(.presentCep(cep))
     }
     
+    private(set) var displayErrorIsCalled: Bool = false
     func displayError(_ message: String) {
-        wasCalled = true
-        howManyTimes += 1
-        expected = message
+        messages.append(.displayError(message))
     }
     
+    private(set) var displayInvalidCepAlertMessageIsCalled: Bool = false
     func displayInvalidCepAlertMessage(_ data: ViaCep.DataCep) {
-        wasCalled = true
-        howManyTimes += 1
-        dataModelExpected = data
+        messages.append(.displayInvalidCepAlertMessage(data))
     }
 }
