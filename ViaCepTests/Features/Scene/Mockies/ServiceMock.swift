1ab93015323f6b24784b4d38da4f5127dd6c6f44
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2024-01-04 18:42:57 -0300

    Did organize line code.

--- ViaCepTests/Features/Scene/Mockies/ServiceMock.swift
+++ ViaCepTests/Features/Scene/Mockies/ServiceMock.swift
@@ -4,10 +4,7 @@ import Foundation
 final class ServiceMock: MainServicing {
     var expexted: [(Result<ViaCep.DataCep, Error>) -> Void] = []
     
-    func fetchDataCep(
-        _ cep: String,
-        callback: @escaping (Result<ViaCep.DataCep, Error>) -> Void
-    ) {
+    func fetchDataCep(_ cep: String, callback: @escaping (Result<ViaCep.DataCep, Error>) -> Void) {
         expexted.append(callback)
     }
 }
