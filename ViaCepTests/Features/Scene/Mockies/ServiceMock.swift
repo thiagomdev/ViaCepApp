08bc9e5f98e90c3d0f668cb013afaa5657d9c74c
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2024-01-04 15:52:04 -0300

    Exchanging expected result from a normal callback to array of expected.

--- ViaCepTests/Features/Scene/Mockies/ServiceMock.swift
+++ ViaCepTests/Features/Scene/Mockies/ServiceMock.swift
@@ -2,10 +2,12 @@ import Foundation
 @testable import ViaCep
 
 final class ServiceMock: MainServicing {
-    var expexted: (Result<ViaCep.DataCep, Error>)?
-    func fetchDataCep(_ cep: String, callback: @escaping (Result<ViaCep.DataCep, Error>) -> Void) {
-        if let expexted = expexted {
-            callback(expexted)
-        }
+    var expexted: [(Result<ViaCep.DataCep, Error>) -> Void] = []
+    
+    func fetchDataCep(
+        _ cep: String,
+        callback: @escaping (Result<ViaCep.DataCep, Error>) -> Void
+    ) {
+        expexted.append(callback)
     }
 }
