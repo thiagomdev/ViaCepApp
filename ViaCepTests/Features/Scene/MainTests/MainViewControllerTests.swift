fe37648afc48f2168112359f39c08e98424660da
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2024-01-04 01:32:25 -0300

    Renamed test, put spy as a private class.

--- ViaCepTests/Features/Scene/MainTests/MainViewControllerTests.swift
+++ ViaCepTests/Features/Scene/MainTests/MainViewControllerTests.swift
@@ -2,8 +2,7 @@ import XCTest
 @testable import ViaCep
 
 final class MainViewControllerTests: XCTestCase {
-    
-    func test_SearchCep_CallsDisplayCepOnInteractor() {
+    func test_searchCep_callsDisplayCepOnInteractor() {
         let (sut, interactorSpy, _) = makeSut()
         let dataCep = DataCep.dummy()
         let cep = dataCep.cep
@@ -12,24 +11,6 @@ final class MainViewControllerTests: XCTestCase {
         sut.searchCep()
 
         XCTAssertEqual(interactorSpy.displayCepExpected, cep)
-    }
-}
-
-final class InteractorSpy: MainInteracting {
-    private(set) var displayCepExpected: String?
-    private(set) var clearTextCep: String?
-    private(set) var displayInvalidCepExpected: DataCep?
-    
-    func displayCep(_ cep: String) {
-        displayCepExpected = cep
-    }
-    
-    func clearText() -> String? {
-        return clearTextCep
-    }
-    
-    func displayInvalidCep(_ data: ViaCep.DataCep) {
-        displayInvalidCepExpected = data
     }
 }
 
@@ -43,5 +24,23 @@ extension MainViewControllerTests {
         let serviceMock = ServiceMock()
         let sut = MainViewController(interactor: interactorSpy)
         return (sut, interactorSpy, serviceMock)
+    }
+    
+    private class InteractorSpy: MainInteracting {
+        private(set) var displayCepExpected: String?
+        private(set) var clearTextCep: String?
+        private(set) var displayInvalidCepExpected: DataCep?
+        
+        func displayCep(_ cep: String) {
+            displayCepExpected = cep
+        }
+        
+        func clearText() -> String? {
+            return clearTextCep
+        }
+        
+        func displayInvalidCep(_ data: ViaCep.DataCep) {
+            displayInvalidCepExpected = data
+        }
     }
 }
