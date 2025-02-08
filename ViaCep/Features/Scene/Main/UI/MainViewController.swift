64c16acc933b3e672bc376015e5c2b8b2a80613f
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2025-02-08 16:00:36 -0300

    - Extract showAlert func to MainViewController extension

--- ViaCep/Features/Scene/Main/UI/MainViewController.swift
+++ ViaCep/Features/Scene/Main/UI/MainViewController.swift
@@ -22,7 +22,9 @@ public final class MainViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+}
+
+extension MainViewController {
     private func showAlert() {
         main.alert = { [weak self] cep in
             guard let self else { return }
